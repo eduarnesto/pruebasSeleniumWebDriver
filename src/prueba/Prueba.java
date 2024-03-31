395791c1f8707a8561d44367e0f4cd15886e3040
elenarivero <elena.rivero@iesnervion.es>
2024-03-31 17:41:10 +0200

    Proyecto de prueba para Selenium

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,16 @@
+package prueba;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        
+	}
+
+}
