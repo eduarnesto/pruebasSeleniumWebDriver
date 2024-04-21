e09a4f9d6696cabbc8d06166b5b5d793f6fb17be
elenarivero <elena.rivero@iesnervion.es>
2024-04-21 19:17:47 +0200

    Prueba básica

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -1,6 +1,8 @@
 package prueba;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Prueba {
@@ -10,7 +12,10 @@ public class Prueba {
 
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
         
+        textBox.sendKeys("Selenium");
 	}
 
 }
